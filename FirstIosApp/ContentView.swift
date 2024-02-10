bd38a7a5e259f1155e6d7da4ce63c707d43c2979
af-liu <18264480900@163.com>
2024-02-10 22:13:53 +0800

    Initial Commit

---
+++ FirstIosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstIosApp
+//
+//  Created by 刘孝文 on 2024/2/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstIosApp/FirstIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIosAppApp.swift
+//  FirstIosApp
+//
+//  Created by 刘孝文 on 2024/2/10.
+//
+
+import SwiftUI
+
+@main
+struct FirstIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
