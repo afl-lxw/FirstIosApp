187b23106db397a650d844b8a09879c34304ab18
af-liu <18264480900@163.com>
2024-02-11 20:04:06 +0800

    feat: first App

--- FirstIosApp/ContentView.swift
+++ FirstIosApp/ContentView.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
+func getText() -> String {
+    return[
+        "👏", "🍎", "🍌" ,"🌰" ,"🍉","🌟"  ].randomElement()!
+}
+
 struct ContentView: View {
+    @State private var textVal = getText()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Text("\(textVal)")
+            Button("点击我起飞"){
+                textVal = getText()
+            }.padding()
             Text("Hello!")
         }
         .padding()
